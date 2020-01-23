dd0e3b585784abee924f0a111b435f7a847e25d1
Rajesh Gupta <rguptan@yahoo.com>
2020-01-23 23:49:19 +0000

    derive tag information
    Added new field in the enex <tag>. The script converts the 3rd part of the tag entry in the *.note file to tag in enex.

--- Tomboy2Evernote.py
+++ Tomboy2Evernote.py
@@ -15,16 +15,18 @@ def process_files(inputdir, outputdir):
     for file in glob.glob("*.note"):
         note_file_path = inputdir + '/' + file
         note_body = open(note_file_path, 'r').read()
+        tag = get_tag(note_body)
+        print(tag)
         title = get_title(note_body)
         html_note_body = get_html_body(note_body)
         created_date = tomboy_to_enex_date(get_created_date(note_body))
         updated_date = tomboy_to_enex_date(get_updated_date(note_body))
-        enex_notes.append(make_enex(title, html_note_body, created_date, updated_date))
+        enex_notes.append(make_enex(title, html_note_body, created_date, updated_date, tag))
         i += 1
     multi_enex_body = make_multi_enex(enex_notes)
     save_to_file(outputdir, output_filename, multi_enex_body)
-    print "Exported notes count: " + `i`
-    print "Evernote file location: " + outputdir + "/" + output_filename
+    print("Exported notes count: ",i)
+    print("Evernote file location: " + outputdir + "/" + output_filename)
 
 
 def get_title(note_body):
@@ -57,9 +59,21 @@ def tomboy_to_enex_date(tomboy_date):
     return re.sub(r"^([0-9]{4})-([0-9]{2})-([0-9]{2})T([0-9]{2}):([0-9]{2}):([0-9]{2}).*", r"\1\2\3T\4\5\6Z",
                   tomboy_date)
 
+def get_tag(note_body):
+    created_date_regex = re.compile("<tag>(.+?)</tag>")
+    matches = created_date_regex.search(note_body);
+    if matches:
+        parts = matches.group(1).split(':')
+        if(len(parts) == 3):
+            return parts[2]
+        else:
+            return "NA"
+    else:
+       return "NA"
+
 
 def get_html_body(note_body):
-    new_line = '¬BR¬'
+    new_line = '<BR/>'
     xml_tag = r"<(\/?)[a-zA-Z0-9_\-:]+>"
     start_xml_tag = r"<[a-zA-Z0-9_\-:]+>"
 
@@ -121,13 +135,13 @@ def get_html_body(note_body):
     return note_body
 
 
-def make_enex(title, body, created_date, updated_date):
+def make_enex(title, body, created_date, updated_date, tag):
     return '''<note><title>''' + title + '''</title><content><![CDATA[<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">
 
 <en-note style="word-wrap: break-word; -webkit-nbsp-mode: space; -webkit-line-break: after-white-space;">
 ''' + body + '''
-</en-note>]]></content><created>''' + created_date + '''</created><updated>''' + updated_date + '''</updated></note>'''
+</en-note>]]></content><created>''' + created_date + '''</created><updated>''' + updated_date + '''</updated><tag>''' + tag + '''</tag></note>'''
 
 
 def make_multi_enex(multi_enex_body):
@@ -146,7 +160,7 @@ def save_to_file(outputdir, filename, body):
 
 
 def get_help_line():
-    print 'Usage: ', sys.argv[0], ' -i <inputdir> -o <outputdir>'
+    print('Usage: ', sys.argv[0], ' -i <inputdir> -o <outputdir>')
 
 
 def get_input_params(argv):
@@ -158,6 +172,7 @@ def get_input_params(argv):
     except getopt.GetoptError:
         exit_with_error()
     for opt, arg in opts:
+        print(opt + ':' + arg)
         if opt == '-h':
             get_help_line()
             sys.exit()
@@ -166,10 +181,10 @@ def get_input_params(argv):
         elif opt in ("-o", "--odir"):
             outputdir = arg
     if (inputdir == ""):
-        print "Error: Missing input folder"
+        print("Error: Missing input folder")
         printhelpline = 1
     if (outputdir == ""):
-        print "Error: Missing output folder"
+        print("Error: Missing output folder")
         printhelpline = 1
     if printhelpline == 1:
         exit_with_error()
@@ -188,4 +203,3 @@ def main(argv):
 
 if __name__ == "__main__":
     main(sys.argv[1:])
-
